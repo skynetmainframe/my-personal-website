61b10bcd534d814b01bb164977c77866f2b64702
Stefan Kohnen <stefan.kohnen@googlemail.com>
2026-03-31 14:16:59 +0200

    Placeholder and frontend functionality for Uploading a song.

--- src/app/song/song.ts
+++ src/app/song/song.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,7 +6,50 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <h2> Song Component </h2>
+    <button (click)="fileInput.click()">Upload song</button>
+    <input
+      type="file"
+      #fileInput
+      accept=".wav"
+      (change)="onFileSelected($event)"
+      hidden
+    />
+
+    <audio *ngIf="audioUrl" controls>
+      <source [src]="audioUrl" type="audio/wav" />
+      Your browser does not support the audio element.
+    </audio>    
   `,
   styleUrl: './song.css',
 })
-export class Song {}
+export class Song {
+    selectedFile: File | null = null;
+  audioUrl: string | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+
+    if (file && file.type === 'audio/wav') {
+      this.selectedFile = file;
+      this.uploadFile();
+    } else {
+      alert('Please select a WAV file');
+    }
+  }
+
+  uploadFile() {
+    if (!this.selectedFile) return;
+
+    const formData = new FormData();
+    formData.append('file', this.selectedFile);
+
+    this.http.post('http://localhost:8080/upload', formData, {
+      responseType: 'text'
+    }).subscribe((response: string) => {
+      // backend returns file URL
+      this.audioUrl = response;
+    });
+  }
+}
